b0a7c04af4dd84204cfb1376774fea0f109641dd
Camille Spain <camille@camillespain.com>
2024-10-28 16:25:15 -0700

    make lint configs ignore dist directories, eslint config add browser globals

--- eslint.config.js
+++ eslint.config.js
@@ -7,14 +7,17 @@ import globals from 'globals';
 import jsdoc from 'eslint-plugin-jsdoc';
 
 export default tseslint.config(
-  { ignores: ['**/env.d.ts'] },
+  { ignores: ['**/env.d.ts', '**/modules/**/*'] },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   jsdoc.configs['flat/recommended-typescript'],
   eslintConfigPrettier,
   {
     languageOptions: {
-      globals: globals.node,
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
     },
     plugins: {
       jsdoc,
